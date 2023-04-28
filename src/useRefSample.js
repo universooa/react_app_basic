5f1d0412ad1cc931900451f8084cfbc2df1ef0a7
sualee <rkttndk@naver.com>
2023-04-28 18:05:39 +0900

    [feat]using useReducer

--- src/useRefSample.js
+++ src/useRefSample.js
@@ -1,27 +1,19 @@
 import UserList from './UserList'
-import { useCallback, useMemo, useRef, useState } from 'react'
+import { useCallback, useMemo, useReducer, useRef, useState } from 'react'
 import CreateUser from './CreateUser'
+import { func } from 'prop-types'
 
 function countActiveUsers(users) {
     console.log('활성 사용자 수를 세는 중..')
     return users.filter((user) => user.active).length
 }
 
-function useRefSample() {
-    const [inputs, setInputs] = useState({
+const initialState = {
+    inputs: {
         username: '',
         email: '',
-    })
-    const { username, email } = inputs
-    const onChange = useCallback((e) => {
-        const { name, value } = e.target
-        setInputs((inputs) => ({
-            ...inputs,
-            [name]: value,
-        }))
-    }, [])
-
-    const [users, setUsers] = useState([
+    },
+    users: [
         {
             id: 1,
             username: 'sualee',
@@ -40,44 +32,88 @@ function useRefSample() {
             email: 'moon@naver.com',
             active: false,
         },
-    ])
+    ],
+}
 
-    const nextId = useRef(4) // 이 값이 current 가 됨.
-    const onCreate = useCallback(() => {
-        const user = {
-            id: nextId.current,
-            username,
-            email,
-        }
+function reducer(state, action) {
+    switch (action.type) {
+        case 'CHANGE_INPUT':
+            return {
+                ...state,
+                inputs: {
+                    ...state.inputs,
+                    [action.name]: action.value,
+                },
+            }
 
-        setUsers((users) => users.concat(user)) // 또는 setUsers(users.concat(user));
-        // 배열은 불변성을 지켜주어야함.
+        case 'CREATE_USER':
+            return {
+                inputs: initialState.inputs,
+                users: state.users.concat(action.user),
+            }
 
-        setInputs({
-            username: '',
-            email: '',
+        case 'TOGGLE_USER':
+            return {
+                ...state,
+                users: state.users.map((user) =>
+                    user.id === action.id
+                        ? { ...user, active: !user.active }
+                        : user
+                ),
+            }
+        case 'REMOVE_USER':
+            return {
+                ...state,
+                users: state.users.filter((user) => user.id !== action.id),
+            }
+        default:
+            return state
+    }
+}
+
+function useRefSample() {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const nextId = useRef(4)
+
+    const { users } = state
+    const { username, email } = state.inputs
+
+    const onChange = useCallback((e) => {
+        const { name, value } = e.target
+        dispatch({
+            type: 'CHANGE_INPUT',
+            name,
+            value,
         })
+    }, [])
 
+    const onCreate = useCallback(() => {
+        dispatch({
+            type: 'CREATE_USER',
+            user: {
+                id: nextId.current,
+                username,
+                email,
+            },
+        })
         nextId.current += 1
     }, [username, email])
 
-    const onRemove = useCallback((id) => {
-        // 배열 불변성 유지 위해 특정 조건이 만족하는 원소들만 추출하여 새로운 배열을 만들어줌
-        setUsers((users) => users.filter((user) => user.id !== id))
+    const onToggle = useCallback((id) => {
+        dispatch({
+            type: 'TOGGLE_USER',
+            id,
+        })
     }, [])
 
-    const onToggle = useCallback((id) => {
-        setUsers((users) =>
-            users.map((user) =>
-                user.id === id ? { ...user, active: !user.active } : user
-            )
-        )
+    const onRemove = useCallback((id) => {
+        dispatch({
+            type: 'REMOVE_USER',
+            id,
+        })
     }, [])
 
     const count = useMemo(() => countActiveUsers(users), [users])
-
-    /* deps 배열 안에 넣은 내용이 바뀌면 우리가 등록한 함수를 호출해서 값을 연산해주고,
-    만약에 내용이 바뀌지 않았다면 이전에 연산한 값을 재사용 */
 
     return (
         <>
@@ -88,7 +124,7 @@ function useRefSample() {
                 onCreate={onCreate}
             />
 
-            <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+            <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
             <div>활성 사용자 수 :{count}</div>
         </>
     )
