45513f5a591d5016f97f2c076a0547546adfaaf2
sualee <rkttndk@naver.com>
2023-04-28 13:43:52 +0900

    [feat]register user

--- src/useRefSample.js
+++ src/useRefSample.js
@@ -1,8 +1,22 @@
 import UserList from './UserList'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
+import CreateUser from './CreateUser'
 
 function useRefSample() {
-    const users = [
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: '',
+    })
+    const { username, email } = inputs
+    const onChange = (e) => {
+        const { name, value } = e.target
+        setInputs({
+            ...inputs,
+            [name]: value,
+        })
+    }
+
+    const [users, setUsers] = useState([
         {
             id: 1,
             username: 'sualee',
@@ -18,14 +32,39 @@ function useRefSample() {
             username: 'moon',
             email: 'moon@naver.com',
         },
-    ]
+    ])
 
     const nextId = useRef(4) // 이 값이 current 가 됨.
     const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username,
+            email,
+        }
+
+        setUsers([...users, user]) // 또는 setUsers(users.concat(user));
+        // 배열은 불변성을 지켜주어야함.
+
+        setInputs({
+            username: '',
+            email: '',
+        })
+
         nextId.current += 1
     }
 
-    return <UserList users={users} />
+    return (
+        <>
+            <CreateUser
+                username={username}
+                email={email}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+
+            <UserList users={users} />
+        </>
+    )
 }
 
 export default useRefSample
