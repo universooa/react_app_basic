f79adf208ce410df5096d1477c6a434b6b99333f
sualee <rkttndk@naver.com>
2023-05-08 18:04:45 +0900

    [feat] useAsync Custom Hook

--- src/Users.js
+++ src/Users.js
@@ -1,54 +1,16 @@
 import { useEffect, useReducer, useState } from 'react'
 import axios from 'axios'
+import useAsync from './useAsync'
 
-function reducer(state, action) {
-    switch (action.type) {
-        case 'LOADING':
-            return {
-                loading: true,
-                data: null,
-                error: null,
-            }
-        case 'SUCCESS':
-            return {
-                loading: false,
-                data: action.data,
-                error: null,
-            }
-        case 'ERROR':
-            return {
-                loading: false,
-                data: null,
-                error: action.error,
-            }
-
-        default:
-            throw new Error(`Unhandled action type: ${action.type}`)
-    }
+async function getUsers() {
+    const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+    )
+    return response.data
 }
 
 function Users() {
-    const [state, dispatch] = useReducer(reducer, {
-        loading: false,
-        data: null,
-        error: null,
-    })
-    const fetchUsers = async () => {
-        dispatch({ type: 'LOADING' })
-        try {
-            const response = await axios.get(
-                'https://jsonplaceholder.typicode.com/users'
-            )
-            dispatch({ type: 'SUCCESS', data: response.data })
-        } catch (e) {
-            dispatch({ type: 'ERROR', error: e })
-        }
-    }
-
-    useEffect(() => {
-        fetchUsers()
-    }, [])
-
+    const [state, refetch] = useAsync(getUsers, [])
     const { loading, data: users, error } = state // state.data를 users 키워드로 조회
 
     if (loading) return <div>로딩 중..</div>
@@ -64,7 +26,7 @@ function Users() {
                     </li>
                 ))}
             </ul>
-            <button type="button" onClick={fetchUsers}>
+            <button type="button" onClick={refetch}>
                 다시 불러오기
             </button>
         </>
