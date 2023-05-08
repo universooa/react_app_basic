2789da729718bffdcc3ad957241774d98f53f2e8
sualee <rkttndk@naver.com>
2023-05-08 17:24:41 +0900

    [feat]add reload button on users

--- src/Users.js
+++ src/Users.js
@@ -5,22 +5,22 @@ function Users() {
     const [users, setUsers] = useState(null)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
+    const fetchUsers = async () => {
+        try {
+            setError(null)
+            setUsers(null)
+            setLoading(true)
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/users'
+            )
+            setUsers(response.data)
+        } catch (e) {
+            setError(e)
+        }
+        setLoading(false)
+    }
 
     useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                setError(null)
-                setUsers(null)
-                setLoading(true)
-                const response = await axios.get(
-                    'https://jsonplaceholder.typicode.com/users'
-                )
-                setUsers(response.data)
-            } catch (e) {
-                setError(e)
-            }
-            setLoading(false)
-        }
         fetchUsers()
     }, [])
 
@@ -29,14 +29,18 @@ function Users() {
     if (!users) return null
 
     return (
-        <ul>
-            {users.map((user) => (
-                <li key={user.id}>
-                    {user.username}({user.name})
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        {user.username}({user.name})
+                    </li>
+                ))}
+            </ul>
+            <button type="button" onClick={fetchUsers}>
+                다시 불러오기
+            </button>
+        </>
     )
 }
-
 export default Users
