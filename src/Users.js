670a2b8dc6705455494f0386e810b074b8aabacd
sualee <rkttndk@naver.com>
2023-05-08 17:32:09 +0900

    [feat] applying useReducer on Users api test

--- src/Users.js
+++ src/Users.js
@@ -1,28 +1,55 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 import axios from 'axios'
 
+function reducer(state, action) {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                data: null,
+                error: null,
+            }
+        case 'SUCCESS':
+            return {
+                loading: false,
+                data: action.data,
+                error: null,
+            }
+        case 'ERROR':
+            return {
+                loading: false,
+                data: null,
+                error: action.error,
+            }
+
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`)
+    }
+}
+
 function Users() {
-    const [users, setUsers] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
+    const [state, dispatch] = useReducer(reducer, {
+        loading: false,
+        data: null,
+        error: null,
+    })
     const fetchUsers = async () => {
+        dispatch({ type: 'LOADING' })
         try {
-            setError(null)
-            setUsers(null)
-            setLoading(true)
             const response = await axios.get(
                 'https://jsonplaceholder.typicode.com/users'
             )
-            setUsers(response.data)
+            dispatch({ type: 'SUCCESS', data: response.data })
         } catch (e) {
-            setError(e)
+            dispatch({ type: 'ERROR', error: e })
         }
-        setLoading(false)
     }
 
     useEffect(() => {
         fetchUsers()
     }, [])
+
+    const { loading, data: users, error } = state // state.data를 users 키워드로 조회
 
     if (loading) return <div>로딩 중..</div>
     if (error) return <div>에러가 발생했습니다.</div>
