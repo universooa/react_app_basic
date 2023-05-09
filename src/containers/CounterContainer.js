9d6a3c1e5a103bf1124e53111a46447beee080b1
sualee <rkttndk@naver.com>
2023-05-09 17:52:22 +0900

    [refactor] refactoring counter's useSelector using shallowEqual

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,14 +1,17 @@
 import { decrease, increase, setDiff } from '../modules/counter'
 import Counter from '../components/Counter'
-import { useDispatch, useSelector } from 'react-redux'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 
 function CounterContainer() {
     // useSelector는 리덕스 스토어의 상태를 조회하는 Hook
     // state의 값은 store.getState() 함수를 호출했을 때 나타나는 결과물과 동일
-    const { number, diff } = useSelector((state) => ({
-        number: state.counter.number,
-        diff: state.counter.diff,
-    }))
+    const { number, diff } = useSelector(
+        (state) => ({
+            number: state.counter.number,
+            diff: state.counter.diff,
+        }),
+        shallowEqual
+    )
 
     // useDispatch는 리덕스 스토어의 dispatch를 함수에서 사용할 수 있게 해주는 Hook
     const dispatch = useDispatch()
