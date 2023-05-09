cb558af9d7b867047d34c4c7b300335fe1d50f08
sualee <rkttndk@naver.com>
2023-05-09 14:36:50 +0900

    [feat] create redux module

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import * as Sentry from '@sentry/react'
+import rootReducer from './modules'
+import { legacy_createStore as createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 Sentry.init({
     dsn: 'https://6fd01d1e166e40639eea99913e5194c7@o4505112906039296.ingest.sentry.io/4505112907546624',
@@ -15,11 +18,13 @@ Sentry.init({
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 })
 
+const store = createStore(rootReducer)
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    // <React.StrictMode>
-    <App />
-    // </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
 
 // If you want to start measuring performance in your app, pass a function
