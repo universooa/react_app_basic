37f9bc3fc3497a5e4288832b9b0768b080e2b991
sualee <rkttndk@naver.com>
2023-05-10 19:58:56 +0900

    [feat] Don't show loading text on reloading

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -1,23 +1,3 @@
-// Promise에 기반한 Thunk를 만들어주는 함수
-export const createPromiseThunk = (type, promiseCreator) => {
-    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`]
-
-    // 이 함수는 promiseCreator가 단 하나의 파라미터만 받는다는 전제하에 작성되었습니다.
-    // 만약 여러 종류의 파라미터를 전달해야하는 상황에서는 객체 타입의 파라미터를 받아오도록 하면 됩니다.
-    // 예: writeComment({ postId: 1, text: '댓글 내용' });
-    return (param) => async (dispatch) => {
-        // 요청 시작
-        dispatch({ type, param })
-        try {
-            // 결과물의 이름을 payload라는 이름으로 통일시킴
-            const payload = await promiseCreator(param)
-            dispatch({ type: SUCCESS, payload })
-        } catch (e) {
-            dispatch({ type: ERROR, payload: e, error: true })
-        }
-    }
-}
-
 // 리듀서에서 사용할 수 있는 여러 유틸 함수들
 export const reducerUtils = {
     initial: (initialData = null) => ({
@@ -43,17 +23,106 @@ export const reducerUtils = {
     }),
 }
 
+// 특정 id를 처리하는 Thunk 생성함수
+const defaultIdSelector = (param) => param
+export const createPromiseThunkById = (
+    type,
+    promiseCreator,
+    // 파라미터에서 id 를 어떻게 선택 할 지 정의하는 함수입니다.
+    // 기본 값으로는 파라미터를 그대로 id로 사용합니다.
+    // 하지만 만약 파라미터가 { id: 1, details: true } 이런 형태라면
+    // idSelector 를 param => param.id 이런식으로 설정 할 수 있곘죠.
+    idSelector = defaultIdSelector
+) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`]
+
+    return (param) => async (dispatch) => {
+        const id = idSelector(param)
+        dispatch({ type, meta: id })
+        try {
+            const payload = await promiseCreator(param)
+            dispatch({ type: SUCCESS, payload, meta: id })
+        } catch (e) {
+            dispatch({ type: ERROR, error: true, payload: e, meta: id })
+        }
+    }
+}
+
+// id 별로 처리하는 유틸함수
+export const handleAsyncActionsById = (type, key, keepData = false) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`]
+    return (state, action) => {
+        const id = action.meta
+        switch (action.type) {
+            case type:
+                return {
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.loading(
+                            // state[key][id]가 만들어져있지 않을 수도 있으니까 유효성을 먼저 검사 후 data 조회
+                            keepData
+                                ? state[key][id] && state[key][id].data
+                                : null
+                        ),
+                    },
+                }
+            case SUCCESS:
+                return {
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.success(action.payload),
+                    },
+                }
+            case ERROR:
+                return {
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.error(action.payload),
+                    },
+                }
+            default:
+                return state
+        }
+    }
+}
+
+// Promise에 기반한 Thunk를 만들어주는 함수
+export const createPromiseThunk = (type, promiseCreator) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`]
+
+    // 이 함수는 promiseCreator가 단 하나의 파라미터만 받는다는 전제하에 작성되었습니다.
+    // 만약 여러 종류의 파라미터를 전달해야하는 상황에서는 객체 타입의 파라미터를 받아오도록 하면 됩니다.
+    // 예: writeComment({ postId: 1, text: '댓글 내용' });
+    return (param) => async (dispatch) => {
+        // 요청 시작
+        dispatch({ type, param })
+        try {
+            // 결과물의 이름을 payload라는 이름으로 통일시킴
+            const payload = await promiseCreator(param)
+            dispatch({ type: SUCCESS, payload })
+        } catch (e) {
+            dispatch({ type: ERROR, payload: e, error: true })
+        }
+    }
+}
+
 // 비동기 관련 액션들을 처리하는 리듀서를 만들어줍니다.
 // type 은 액션의 타입, key 는 상태의 key (예: posts, post) 입니다.
 
-export const handleAsyncActions = (type, key) => {
+export const handleAsyncActions = (type, key, keepData = false) => {
+    // keepData라는 파라미터를 추가하여 만약 이 값이 true면 로딩을 할 때에도 데이터 유지
     const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`]
     return (state, action) => {
         switch (action.type) {
             case type:
                 return {
                     ...state,
-                    [key]: reducerUtils.loading(),
+                    [key]: reducerUtils.loading(
+                        keepData ? state[key].data : null
+                    ),
                 }
             case SUCCESS:
                 return {
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -4,7 +4,9 @@
 import * as postsAPI from '../api/posts'
 import {
     createPromiseThunk,
+    createPromiseThunkById,
     handleAsyncActions,
+    handleAsyncActionsById,
     reducerUtils,
 } from '../lib/asyncUtils'
 
@@ -16,6 +18,9 @@ const GET_POSTS_ERROR = 'GET_POSTS_ERROR' // 요청 실패
 const GET_POST = 'GET_POST'
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS'
 const GET_POST_ERROR = 'GET_POST_ERROR'
+
+// 포스트 비우기
+const CLEAR_POST = 'CLEAR_POST'
 
 // thunk를 사용할 때, 꼭 모든 액션들에 대하여 액션 생성함수를 만들 필요는 없음
 // 그냥 thunk 함수에서 바로 액션 객체를 만들어도 됨
@@ -31,8 +36,9 @@ const GET_POST_ERROR = 'GET_POST_ERROR'
 // }
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts)
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById)
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById)
 
+export const clearPost = () => ({ type: CLEAR_POST })
 //
 // // thunk함수에서도 파라미터를 받아서 사용할 수 있음
 // export const getPost = (id) => async (dispatch) => {
@@ -47,7 +53,7 @@ export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById)
 
 const initialState = {
     posts: reducerUtils.initial(),
-    post: reducerUtils.initial(),
+    post: {},
 }
 
 export default function posts(state = initialState, action) {
@@ -55,11 +61,16 @@ export default function posts(state = initialState, action) {
         case GET_POSTS:
         case GET_POSTS_SUCCESS:
         case GET_POSTS_ERROR:
-            return handleAsyncActions(GET_POSTS, 'posts')(state, action)
+            return handleAsyncActions(GET_POSTS, 'posts', true)(state, action)
         case GET_POST:
         case GET_POST_SUCCESS:
         case GET_POST_ERROR:
-            return handleAsyncActions(GET_POST, 'post')(state, action)
+            return handleAsyncActionsById(GET_POST, 'post', true)(state, action)
+        // case CLEAR_POST:
+        //     return {
+        //         ...state,
+        //         post: reducerUtils.initial(),
+        //     }
         default:
             return state
     }
