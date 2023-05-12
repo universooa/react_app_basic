c990816ccc9968176423359966ecb51850bc99a2
sualee <rkttndk@naver.com>
2023-05-12 17:02:48 +0900

    [feat] Shape interface on type script

--- dist/practice.js
+++ dist/practice.js
@@ -1,14 +1,31 @@
 'use strict'
-let count = 0
-count += 1
-// count = '갑자기 분위기 문자열'
-const message = 'hello world'
-const done = true
-const numbers = [1, 2, 3]
-const messages = ['hello', 'world']
-// messages.push(1) // 숫자 넣으려고 하면 안됨
-const mightBeUndefined = undefined // string일수도 있고 undefined일수도 있음
-const nullableNumber = null // number일수도 있고 null일수도 있음
-let color = 'red'
-color = 'yellow'
-// color = 'green' //에러
+// eslint-disable-next-line max-classes-per-file
+function sumArray(numbers) {
+    return numbers.reduce((acc, current) => acc + current, 0)
+}
+const total = sumArray([1, 2, 3, 4, 5])
+function returnNothing() {
+    console.log('I am just saying hello world')
+}
+class Circle {
+    constructor(radius) {
+        this.radius = radius
+    }
+    // 너비를 가져오는 함수
+    getArea() {
+        return this.radius * this.radius * Math.PI
+    }
+}
+class Rectangle {
+    constructor(width, height) {
+        this.width = width
+        this.height = height
+    }
+    getArea() {
+        return this.width * this.height
+    }
+}
+const shapes = [new Circle(5), new Rectangle(10, 5)]
+shapes.forEach((shape) => {
+    console.log(shape.getArea())
+})
--- src/practice.ts
+++ src/practice.ts
@@ -1,18 +1,50 @@
-let count = 0
-count += 1
-// count = '갑자기 분위기 문자열'
+// eslint-disable-next-line max-classes-per-file
+function sumArray(numbers: number[]): number {
+    return numbers.reduce((acc, current) => acc + current, 0)
+}
 
-const message: string = 'hello world'
-const done: boolean = true
+const total = sumArray([1, 2, 3, 4, 5])
 
-const numbers: number[] = [1, 2, 3]
-const messages: string[] = ['hello', 'world']
+function returnNothing(): void {
+    console.log('I am just saying hello world')
+}
 
-// messages.push(1) // 숫자 넣으려고 하면 안됨
+// shape이라는 interface를 선언
+interface Shape {
+    getArea(): number // Shape interface에는 getArea라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자임
+}
 
-const mightBeUndefined: string | undefined = undefined // string일수도 있고 undefined일수도 있음
-const nullableNumber: number | null = null // number일수도 있고 null일수도 있음
+class Circle implements Shape {
+    // `implements` 키워드를 사용하여 해당 클래스가 Shape interface의 조건을 충족하겠다는 것을 명시
+    radius: number
 
-let color: 'red' | 'orange' | 'yellow' = 'red'
-color = 'yellow'
-// color = 'green' //에러
+    constructor(radius: number) {
+        this.radius = radius
+    }
+
+    // 너비를 가져오는 함수
+    getArea() {
+        return this.radius * this.radius * Math.PI
+    }
+}
+
+class Rectangle implements Shape {
+    width: number
+
+    height: number
+
+    constructor(width: number, height: number) {
+        this.width = width
+        this.height = height
+    }
+
+    getArea(): number {
+        return this.width * this.height
+    }
+}
+
+const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)]
+
+shapes.forEach((shape) => {
+    console.log(shape.getArea())
+})
