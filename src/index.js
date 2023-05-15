81a65c8ba61b7020b5df64a54a933734ea7b6907
sualee <rkttndk@naver.com>
2023-05-15 16:39:02 +0900

    [ci] make Dockerfile

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import * as Sentry from '@sentry/react'
 import rootReducer, { rootSaga } from './modules'
 import { applyMiddleware, legacy_createStore as createStore } from 'redux'
 import { Provider } from 'react-redux'
@@ -17,16 +16,16 @@ axios.default.baseURL =
     process.env.NODE_ENV === 'development'
         ? '/'
         : 'https://blog.naver.com/rkttndk'
-
-Sentry.init({
-    dsn: 'https://6fd01d1e166e40639eea99913e5194c7@o4505112906039296.ingest.sentry.io/4505112907546624',
-    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-})
+//
+// Sentry.init({
+//     dsn: 'https://6fd01d1e166e40639eea99913e5194c7@o4505112906039296.ingest.sentry.io/4505112907546624',
+//     integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+//     // Performance Monitoring
+//     tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+//     // Session Replay
+//     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// })
 
 const sagaMiddleware = createSagaMiddleware()
 
