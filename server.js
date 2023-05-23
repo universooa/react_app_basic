11ca15b74dff26329c826433a5aaec75321071d1
sualee <rkttndk@naver.com>
2023-05-23 14:15:57 +0900

    [deploy] express server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+const cors = require('cors')
+
+app.use(cors())
+
+app.listen(8080, function () {
+    console.log('listening on 8080')
+})
+
+app.use(express.static(path.join(__dirname, './build')))
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './build/index.html'))
+})
